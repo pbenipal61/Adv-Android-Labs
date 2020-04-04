4571c0161324cd935f4ee5f3181c378c60896b3b
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-04 22:03:55 +0300

    Added activity for lab 2.2 and added intent for the activity

---
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab2_2.java
@@ -0,0 +1,12 @@
+package com.postmaninteractive.adv_android_labs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/postmaninteractive/adv_android_labs/MainActivity.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/MainActivity.java
@@ -1,13 +1,25 @@
 package com.postmaninteractive.adv_android_labs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Button lab22 = (Button) findViewById(R.id.lab_2_2);
+        lab22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Lab2_2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
