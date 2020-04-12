816e05852ed7df5132086865e08b2aeca0cfe728
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-12 18:57:56 +0300

    Finished Lab 2.2

--- app/src/main/java/com/postmaninteractive/adv_android_labs/Lab2_2.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab2_2.java
@@ -3,29 +3,98 @@ package com.postmaninteractive.adv_android_labs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.postmaninteractive.adv_android_labs.Models.Countries;
+
+import java.util.ArrayList;
 
 public class Lab2_2 extends AppCompatActivity {
+
+    private final Countries countries = new Countries();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout mainLayout = new LinearLayout(this);
-        LinearLayout mainButtonsLayout = new LinearLayout(this);
-        LinearLayout.LayoutParams mainButtonsLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        mainButtonsLayout.setLayoutParams(mainButtonsLayoutParams);
-        Button addButton = new Button(this);
-        addButton.setText("Add");
-        mainButtonsLayout.addView(addButton);
-        Button editButton = new Button(this);
-        editButton.setText("Edit");
-        mainButtonsLayout.addView(editButton);
-        Button removeButton = new Button(this);
-        removeButton.setText("Remove");
-        mainButtonsLayout.addView(removeButton);
+        countries.addCountry("Finland");
+        countries.addCountry("Sweden");
+        countries.addCountry("Australia");
+        countries.addCountry("Norway");
+        countries.addCountry("Switzerland");
+        countries.addCountry("Somalia");
+        countries.addCountry("Kenya");
+        countries.addCountry("Brazil");
+        countries.addCountry("Belgium");
+        countries.addCountry("Germany");
 
-        mainLayout.addView(mainButtonsLayout);
+        ArrayList<String> listOfCountries = countries.getCountries();
+
+        //     Creating layouts programmatically
+        LinearLayout verticalLayout = new LinearLayout(this);
+        LinearLayout horizontalLayout = new LinearLayout(this);
+//     Setting their orientations
+        verticalLayout.setOrientation(LinearLayout.VERTICAL);
+        horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+//     Creating buttons programmatically
+        Button addBtn = new Button(this);
+        Button editBtn = new Button(this);
+        Button removeBtn = new Button(this);
+//     Setting text to the buttons
+        addBtn.setText("Add");
+        editBtn.setText("Edit");
+        removeBtn.setText("Remove");
+//     Creating EditText programmatically
+        final EditText editText = new EditText(this);
+
+//     Creating ListView programmatically
+        ListView listView = new ListView(this);
+//     Creating Array Adapter
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listOfCountries
+        );
+
+//     Creating onClickListeners
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  editTextValue = editText.getText().toString();
+                Log.d("onClick", "" + editTextValue);
+                countries.addCountry(editTextValue);
+                arrayAdapter.notifyDataSetChanged();
+                editText.setText("");
+            }
+        });
+
+        removeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  editTextValue = editText.getText().toString();
+                Log.d("onClick", "" + editTextValue);
+                countries.popCountries();
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+//     Populating listView with data
+        listView.setAdapter(arrayAdapter);
+//     Adding UI elements to the layouts
+        verticalLayout.addView(horizontalLayout);
+        horizontalLayout.addView(addBtn);
+        horizontalLayout.addView(editBtn);
+        horizontalLayout.addView(removeBtn);
+        verticalLayout.addView(editText);
+        verticalLayout.addView(listView);
+
+        setContentView(verticalLayout);
     }
-}
+    }
+
