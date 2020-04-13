150ac8dd4239a469c8595d19771a0b1d726cc795
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-13 23:12:32 +0300

    Finished Lab3.4

--- app/src/main/java/com/postmaninteractive/adv_android_labs/Lab3/Lab3_4.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab3/Lab3_4.java
@@ -1,7 +1,9 @@
 package com.postmaninteractive.adv_android_labs.Lab3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,18 +11,23 @@ import android.widget.EditText;
 import com.postmaninteractive.adv_android_labs.R;
 
 public class Lab3_4 extends AppCompatActivity {
-
+    private static final String TAG = "Lab3_4";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab3_4);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
+        final EditText editText = (EditText) findViewById(R.id.editText);
         Button startTimerButton = (Button) findViewById(R.id.startTimerButton);
         startTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String timer = editText.getText().toString();
+                Log.d(TAG, "onClick: " + " " + timer);
+                Intent intent = new Intent(getApplicationContext(), TimerCountdownActivity.class);
+                intent.putExtra("timer", Integer.parseInt(timer));
+                Log.d(TAG, "onClick: here");
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/postmaninteractive/adv_android_labs/Lab3/TimerCountdownActivity.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab3/TimerCountdownActivity.java
@@ -1,15 +1,39 @@
 package com.postmaninteractive.adv_android_labs.Lab3;
 
+import android.annotation.SuppressLint;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.postmaninteractive.adv_android_labs.R;
 
 public class TimerCountdownActivity extends AppCompatActivity {
 
+    private static final String TAG = "TimerCountdownActivity";
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_countdown);
+
+        int preTimer = getIntent().getIntExtra("timer", 30);
+        Log.d(TAG, "onCreate: "+ preTimer);
+
+        long timerAsLong = Long.parseLong(String.valueOf(preTimer* 1000));
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        new CountDownTimer(timerAsLong,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText(String.valueOf(Integer.parseInt(String.valueOf(millisUntilFinished/1000))));
+                counter++;
+            }
+            @Override
+            public void onFinish() {
+                textView.setText("Finished");
+            }
+        }.start();
     }
 }
