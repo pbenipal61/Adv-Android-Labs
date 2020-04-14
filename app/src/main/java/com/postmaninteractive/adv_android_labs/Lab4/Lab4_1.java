6a02406e5ec0f70eab98de11ad22dd063117ddba
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-14 13:02:33 +0300

    Setup for Lab4.1

---
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab4/Lab4_1.java
@@ -0,0 +1,15 @@
+package com.postmaninteractive.adv_android_labs.Lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.postmaninteractive.adv_android_labs.R;
+
+public class Lab4_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab4_1);
+    }
+}
--- app/src/main/java/com/postmaninteractive/adv_android_labs/MainActivity.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/MainActivity.java
@@ -13,6 +13,7 @@ import com.postmaninteractive.adv_android_labs.Lab3.Lab31;
 import com.postmaninteractive.adv_android_labs.Lab3.Lab3_2;
 import com.postmaninteractive.adv_android_labs.Lab3.Lab3_3;
 import com.postmaninteractive.adv_android_labs.Lab3.Lab3_4;
+import com.postmaninteractive.adv_android_labs.Lab4.Lab4_1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,6 +81,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Lab3_4.class);
+                startActivity(intent);
+            }
+        });
+
+        Button lab41 = (Button) findViewById(R.id.lab_4_1);
+        lab41.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Lab4_1.class);
                 startActivity(intent);
             }
         });
