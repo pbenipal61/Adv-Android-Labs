88121e866947f815af727c4b82c623d774b4c7bf
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-04-14 13:11:45 +0300

    Finished Lab4.1

--- app/src/main/java/com/postmaninteractive/adv_android_labs/Lab4/Lab4_1.java
+++ app/src/main/java/com/postmaninteractive/adv_android_labs/Lab4/Lab4_1.java
@@ -2,14 +2,46 @@ package com.postmaninteractive.adv_android_labs.Lab4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.postmaninteractive.adv_android_labs.R;
 
 public class Lab4_1 extends AppCompatActivity {
 
+    private TextView textView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab4_1);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void sendRequest(View view) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, editText.getText().toString(),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("Proper url not provided");
+            }
+        });
+        queue.add(stringRequest);
+
     }
 }
